04022074c5455d3233670b56146b2dc20a9cc341
Shrinath Nimare <shrinath.nimare@gmail.com>
2019-01-30 00:38:04 +0000

    Might not need a content script after all

--- js/content_script.js
+++
@@ -1,7 +0,0 @@
-alert("Running!");
-chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
-  if(request.todo == "REDIRECT")
-  {
-    window.location.href = request.data;
-  }
-});
